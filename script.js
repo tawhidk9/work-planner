4382e0cda94ec51134366292cd6c03dc57fde919
Tawhid <tawhidkamal5@gmail.com>
2023-05-26 01:21:08 -0400

    added html code w/ timeblocks, buttons, linked js/moment/jquery, linked google font, linked bootstrap//css added style template from referenced repo w/ added jumbotron class and style//JS created time display, save button to save inputs

---
+++ script.js
@@ -0,0 +1,9 @@
+//DOM Elements
+var timeBlock = document.querySelector(".container")
+var currentDay = moment();
+
+//Display current date/time with moment formatting
+$("#toDay").text(currentDay.format("LLLL"));
+
+//User Interaction
+
