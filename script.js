8929db30340359136763bafe8cdc6105d9b362a6
Tawhid <tawhidkamal5@gmail.com>
2023-05-28 15:00:56 -0400

    fiddling around alot

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var currentDay = moment();
 $("#today").text(currentDay.format("LLLL"));
 
 //User Interaction
-$("saveBtn").on("click", function(){
+$(".saveBtn").on('click', function(){
     var textInput = $(this).siblings(".text").val(); //grabs all values of text class
     var tIme = $(this).parent().attr("id"); //grabs id of parent div
     localStorage.setItem(textInput, tIme); //saves both to local storage
@@ -18,4 +18,29 @@ $("saveBtn").on("click", function(){
 function colorChange(){
     var currentHour = currentDay.hours();
 
-}
+    $('.time-block').each(function () {
+        var time = parseInt($(this).attr('id').split("time")[1]);
+    
+        // if the time Id attribute is before the current hour, add the past class
+        if (time < currentHour) {
+          $(this).addClass('past');
+        } // if the time Id attribute is equal to the current hour, remove the past and future classes and add the present class
+        else if (time === currentHour) {
+          $(this).addClass('present');
+          $(this).removeClass('past');
+        } // If the time Id attribute is greater than the current time, remove the past and present classes and add the future class
+        else {
+          $(this).addClass('future');
+          $(this).removeClass('past');
+          $(this).removeClass('present');
+        }
+      });
+    }
+    
+    colorChange();
+    
+    setInterval(function () {
+      colorChange();
+    }, 1000 * 60);
+
+
